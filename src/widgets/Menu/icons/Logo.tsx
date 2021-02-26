c59bade9ef1b7382c29a3bb10ccfef608e00d279
swebexport <iwan930409@outlook.com>
2021-02-26 20:28:14 +0900

    change Logo v0.2

--- src/widgets/Menu/icons/Logo.tsx
+++ src/widgets/Menu/icons/Logo.tsx
@@ -10,7 +10,8 @@ const Logo: React.FC<LogoProps> = ({ isDark, ...props }) => {
   const textColor = isDark ? "#FFFFFF" : "#000000";
   return (
     <Svg viewBox="0 0 160 26" {...props}>
-      <image width="205" height="26" href={isDark ? 'images/LogoTextNewDark.png' : 'images/LogoTextNewWhite.png'}/>
+      <image width={26} height={26} href={isDark ? 'images/LogoTextNewDark.png' : 'images/LogoTextNewWhite.png'}/>
+      <p color={textColor}>InstanceSwap</p>
     </Svg>
   );
 };
